a4ea7986833b3a32e60506ff04a4eb9b23acad99
Vladislav <vladislav_chernikov94@mail.ru>
2022-11-23 04:57:58 +0700

    дз урок 1 задача 1

--- exemple4/Program.cs
+++ exemple4/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+int numberA = new Random().Next(-1000, 1000);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(-1000, 1000);
+Console.WriteLine(numberB);
+if (numberA > numberB)
+{
+    Console.WriteLine(numberA);
+}
+else
+{
+    Console.WriteLine(numberB);
+}
+
+    
+   
